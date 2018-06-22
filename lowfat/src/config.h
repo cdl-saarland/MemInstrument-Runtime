4a8e3db580aba29f7d1e73c5d97cccdef3e6d915
Philip Gebel <s8phgebe@stud.uni-saarland.de>
2018-06-22 10:20:23 +0200

    NUM_REGIONS is now correctly set depending on FAST_BASE or POW_2_SIZES

--- lowfat/src/config.h
+++ lowfat/src/config.h
@@ -1,15 +1,16 @@
 #pragma once
 
 #define REGION_SIZE 0x100000000ULL // 0x100000000 for 4GB
+#define REGION_SIZE_LOG 32U // binary log of REGION_SIZE, if region size is changed, this must be adapted as well
 
-// binary log of REGION_SIZE, if region size is changed, this must be adapted correspondingly
-#define REGION_SIZE_LOG 32U
-
-// TODO maybe replace this with the size of sizes[]
-#define NUM_REGIONS 529U
-
-// multiply with (stored) inverse of size
+// multiply with (pre calculated) inverse of size
 #define FAST_BASE
 
 // faster size index computation if sizes only contains powers of 2
 //#define POW_2_SIZES
+
+#ifdef POW_2_SIZES
+#define NUM_REGIONS 27U
+#else
+#define NUM_REGIONS 529U
+#endif
