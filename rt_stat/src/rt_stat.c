e1d5d3f0adcf38c7203b54f2b41f2b9a3fba9700
Fabian Ritter <fabian.ritter@cs.uni-saarland.de>
2018-08-30 11:48:26 +0200

    Format rt_stat sources

--- rt_stat/src/rt_stat.c
+++ rt_stat/src/rt_stat.c
@@ -1,9 +1,11 @@
+#include "rt_stat.h"
+
 #include <assert.h>
+#include <execinfo.h>
 #include <stddef.h>
 #include <stdint.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <execinfo.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 #define __USE_GNU
@@ -11,24 +13,24 @@
 
 #define STATS_FILE "MI_STATS_FILE"
 
-static const char* mi_prog_name = NULL;
+static const char *mi_prog_name = NULL;
 
-static const char* mi_stats_file = NULL;
+static const char *mi_stats_file = NULL;
 
 typedef struct {
     uint64_t val;
-    const char* text;
+    const char *text;
 } StatType;
 
 typedef struct {
-    StatType* table;
+    StatType *table;
     uint64_t num_elems;
 } StatTableType;
 
 /**
  * A table of stat tables
  */
-static StatTableType* __StatTable = NULL;
+static StatTableType *__StatTable = NULL;
 
 static uint64_t __NumStatTables = 0;
 
@@ -41,11 +43,12 @@ uint64_t __mi_stat_init(uint64_t num) {
     if (__StatTable == NULL) {
         __StatTable = malloc(sizeof(StatTableType));
     } else {
-        __StatTable = realloc(__StatTable, sizeof(StatTableType) * (__NumStatTables + 1));
+        __StatTable =
+            realloc(__StatTable, sizeof(StatTableType) * (__NumStatTables + 1));
     }
     ++__NumStatTables;
 
-    StatTableType* newElem = &__StatTable[__NumStatTables - 1];
+    StatTableType *newElem = &__StatTable[__NumStatTables - 1];
 
     newElem->num_elems = num;
     newElem->table = malloc(sizeof(StatType) * num);
@@ -53,9 +56,9 @@ uint64_t __mi_stat_init(uint64_t num) {
     return __NumStatTables - 1;
 }
 
-void __mi_stat_init_entry(uint64_t tableID, uint64_t idx, const char* text) {
+void __mi_stat_init_entry(uint64_t tableID, uint64_t idx, const char *text) {
     assert(tableID < __NumStatTables);
-    StatTableType* entry = &__StatTable[tableID];
+    StatTableType *entry = &__StatTable[tableID];
     assert(idx < entry->num_elems);
     entry->table[idx].val = 0;
     entry->table[idx].text = text;
@@ -63,13 +66,13 @@ void __mi_stat_init_entry(uint64_t tableID, uint64_t idx, const char* text) {
 
 void __mi_stat_inc(uint64_t tableID, uint64_t idx) {
     assert(tableID < __NumStatTables);
-    StatTableType* entry = &__StatTable[tableID];
+    StatTableType *entry = &__StatTable[tableID];
     assert(idx < entry->num_elems);
     ++entry->table[idx].val;
 }
 
 void __mi_print_stats(void) {
-    FILE* dest = stderr;
+    FILE *dest = stderr;
     if (mi_prog_name == NULL) {
         mi_prog_name = "[name not set]";
     }
@@ -83,15 +86,17 @@ void __mi_print_stats(void) {
     fprintf(dest, "\n==================================================\n");
 
     if (__StatTable == NULL) {
-        fprintf(dest, "no meminstrument stat table allocated for '%s'!\n", mi_prog_name);
+        fprintf(dest, "no meminstrument stat table allocated for '%s'!\n",
+                mi_prog_name);
     } else {
         fprintf(dest, "meminstrument runtime stats for '%s':\n", mi_prog_name);
 
         for (uint64_t i = 0; i < __NumStatTables; ++i) {
-            StatTableType* entry = &__StatTable[i];
+            StatTableType *entry = &__StatTable[i];
             uint64_t bnd = entry->num_elems;
             for (uint64_t j = 0; j < bnd; ++j) {
-                fprintf(dest, "STAT  %s : %lu\n", entry->table[j].text, entry->table[j].val);
+                fprintf(dest, "STAT  %s : %lu\n", entry->table[j].text,
+                        entry->table[j].val);
             }
         }
     }
@@ -99,8 +104,12 @@ void __mi_print_stats(void) {
     fclose(dest);
 }
 
-typedef int (*fcn)(int *(main)(int, char **, char **), int argc, char **ubp_av, void (*init)(void), void (*fini) (void), void (*rtld_fini)(void), void (*stack_end));
-int __libc_start_main(int *(main) (int, char **, char **), int argc, char **ubp_av, void (*init)(void), void (*fini)(void), void (*rtld_fini)(void), void (* stack_end)) {
+typedef int (*fcn)(int *(main)(int, char **, char **), int argc, char **ubp_av,
+                   void (*init)(void), void (*fini)(void),
+                   void (*rtld_fini)(void), void(*stack_end));
+int __libc_start_main(int *(main)(int, char **, char **), int argc,
+                      char **ubp_av, void (*init)(void), void (*fini)(void),
+                      void (*rtld_fini)(void), void(*stack_end)) {
 
     mi_prog_name = ubp_av[0];
 
@@ -109,10 +118,10 @@ int __libc_start_main(int *(main) (int, char **, char **), int argc, char **ubp_
 #endif
 
     if (atexit(__mi_print_stats) != 0) {
-        fprintf(stderr, "meminstrument: Failed to register statistics printer!\n");
+        fprintf(stderr,
+                "meminstrument: Failed to register statistics printer!\n");
     }
 
     fcn start = (fcn)dlsym(RTLD_NEXT, "__libc_start_main");
     return (*start)(main, argc, ubp_av, init, fini, rtld_fini, stack_end);
 }
-
