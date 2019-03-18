42c95e440fd929ffc213e843b2b76540c4c9553f
Fabian Ritter <fabian.ritter@cs.uni-saarland.de>
2019-03-18 12:09:28 +0100

    Use mi_stats.txt as the default statistics file name

--- splay/src/config.h
+++ splay/src/config.h
@@ -47,7 +47,11 @@
 #define STATS_COUNTER_DEFS "statistic_counters.def"
 
 /// If defined, use this key to find a file to print runtime stats to
-#define STATS_FILE "MI_STATS_FILE"
+// #define STATS_FILE_ENV "MI_STATS_FILE"
+
+/// If defined and no STATS_FILE_ENV defined, use this as the name of the file
+/// to print statistics to.
+#define STATS_FILE "mi_stats.txt"
 
 /// Enable Intel MPX bound checking on startup if defined to 1
 // #define ENABLE_MPX 1
