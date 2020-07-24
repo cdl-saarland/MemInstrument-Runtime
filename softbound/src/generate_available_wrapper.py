66034991b2b7f509a9b33a873438b05b56601b5f
Tina Jung <t.jung@cs.uni-saarland.de>
2020-07-24 14:42:16 +0200

    Adapt auto header generation script to make some parts more generic

--- softbound/src/generate_available_wrapper.py
+++ softbound/src/generate_available_wrapper.py
@@ -3,75 +3,123 @@
 import os
 import argparse
 import subprocess
-
-from shutil import which
-from shutil import rmtree
+import math
+import textwrap
+import sys
+import shutil
 
 def is_available(program_name):
-    return which(program_name) is not None
+    """Check if a given program is available."""
+    return shutil.which(program_name) is not None
 
-def surround_with_C_header_stuff(text, number_of_functions):
-    header_name = "SBMI_WRAPPER_H"
-    header_text =  "//===-------- SBMI/Wrapper.h - Supported Wrapper Functions ------*- C++ -*-===//" + "\n"
-    header_text += "//" + "\n"
-    header_text += "//                     The LLVM Compiler Infrastructure" + "\n"
-    header_text += "//" + "\n"
-    header_text += "// This file is distributed under the University of Illinois Open Source" + "\n"
-    header_text += "// License. See LICENSE.TXT for details." + "\n"
-    header_text += "//" + "\n"
-    header_text += "//===----------------------------------------------------------------------===//" + "\n"
-    header_text += "///" + "\n"
-    header_text += "/// \\file" + "\n"
-    header_text += "/// Contains all wrapper functions available in the SBMI runtime." + "\n"
-    header_text += "///" + "\n"
-    header_text += "//===----------------------------------------------------------------------===//" + "\n\n"
-    header_text += "#ifndef " + header_name + "\n"
-    header_text += "#define " + header_name + "\n\n"
-    header_text += '#include "llvm/ADT/SmallVector.h"\n\n'
-    header_text += "namespace sbmi {\n\n"
-    header_text += 'llvm::SmallVector<String, ' + str(number_of_functions) + '> available_wrappers = {\n'
-    header_text += text + "}\n\n"
-    header_text += "} // end namespace sbmi\n\n"
-    header_text += "#endif // " + header_name + "\n"
 
+def generate_header(header_name, file_path, includes, namespace, content,
+                    short_description, long_description):
+    """
+    Generate the content of an LLVM C++ header, given all non-generic details as arguments.
+    """
+    # Calculate the line filler of the first line
+    first_line_length = len(
+        f"//=== {file_path} - {short_description} *- C++ -*-===//")
+    fill_num = 80 - first_line_length
+    if fill_num <= 0:
+        print(
+            f"File path and short description are too long (by {abs(fill_num)} characters)")
+        sys.exit(1)
+    minus_front = "-" * math.floor(fill_num / 2.0)
+    minus_back = "-" * math.ceil(fill_num / 2.0)
+
+    header_text = textwrap.dedent(f"""\
+    //==={minus_front} {file_path} - {short_description} {minus_back}*- C++ -*-===//
+    //
+    //                     The LLVM Compiler Infrastructure
+    //
+    // This file is distributed under the University of Illinois Open Source
+    // License. See LICENSE.TXT for details.
+    //
+    //===----------------------------------------------------------------------===//
+    ///
+    /// \\file
+    /// {long_description}
+    /// THIS FILE IS AUTO-GENERATED. DO NOT UPDATE IT MANUALLY.
+    ///
+    //===----------------------------------------------------------------------===//
+
+    #ifndef {header_name}
+    #define {header_name}
+
+    {includes}
+
+    namespace {namespace} {{
+
+    """) + content + textwrap.dedent(
+        f"""
+        }} // end namespace {namespace}
+
+        #endif // {header_name}
+        """)
     return header_text
 
-def process_file(file_name, verbose):
+def surround_with_header_stuff(text, number_of_functions, out_file_name):
+    """
+    Specify the header content and use it to instantiate the LLVM header template.
+    """
+    header_name = "SB_WRAPPER_H"
+    file_path = "SBMI/" + out_file_name
+    namespace = "sbmi"
+    short_description = "Supported Wrapper Functions"
+    long_description = "Contains all wrapper functions available in the SBMI runtime."
+    includes = '#include "llvm/ADT/SmallVector.h"'
+    content = "llvm::SmallVector<String, " + \
+        str(number_of_functions) + "> available_wrappers = {\n" + text + "}"
 
+    return generate_header(header_name, file_path, includes, namespace, content,
+                           short_description, long_description)
+
+def process_file(file_name, out_file_name, verbose):
+    """
+    Filter all SoftBound function from the given ctags file and generate the C++ header.
+    """
     number_of_functions = 0
     result = ''
     with open(file_name, "r") as open_file:
-       for line in open_file:
+        for line in open_file:
 
-           # ignore ctags metadata
-           if line.startswith("!"):
-               if verbose:
-                   print("Ignore ctag metadata:\t" + line)
-               continue
+            # ignore ctags metadata
+            if line.startswith("!"):
+                if verbose:
+                    print("Ignore ctag metadata:\t" + line)
+                continue
 
             # ignore helper functions
-           if (not line.startswith("softboundcets")) and (not line.startswith("__softboundcets")):
-               if verbose:
-                   print("Ignore softbound helper function:\t" + line)
-               continue
+            if (not line.startswith("softboundcets")) and (not line.startswith("__softboundcets")):
+                if verbose:
+                    print("Ignore softbound helper function:\t" + line)
+                continue
 
-           splitted = line.split("\t")
-           function_name = splitted[0]
-           result += '\t"' + function_name + '",\n'
-           number_of_functions += 1
+            splitted = line.split("\t")
+            function_name = splitted[0]
+            result += '\t"' + function_name + '",\n'
+            number_of_functions += 1
 
-    result = surround_with_C_header_stuff(result, number_of_functions)
+    result = surround_with_header_stuff(result, number_of_functions, out_file_name)
     return result
 
-def generate_file(list_of_source_files, verbose):
 
+def generate_file(list_of_source_files, out_file_name, verbose):
+    """
+    Find SoftBound function in the given source files and generate a C++ header
+    that lists the functions.
+    """
     tmp_file_name = "ctags.out"
     if not is_available("ctags"):
-        print("Ctags is necessary for this program to work. Please install it (you can find it here: http://ctags.sourceforge.net/).")
-        exit(1)
+        print("Ctags is necessary for this program to work."
+              "Please install it (you can find it here: http://ctags.sourceforge.net/).")
+        sys.exit(1)
 
-    result = subprocess.run(["ctags", "-o", tmp_file_name, "--c-types=f"] + list_of_source_files, check=True)
-    c_file_content = process_file(tmp_file_name, verbose)
+    subprocess.run(["ctags", "-o", tmp_file_name, "--c-types=f"] +
+                   list_of_source_files, check=True)
+    c_file_content = process_file(tmp_file_name, out_file_name, verbose)
 
     # Delete the temporarily generated file
     os.remove(tmp_file_name)
@@ -80,20 +128,30 @@ def generate_file(list_of_source_files, verbose):
         print("Header file content:")
         print(c_file_content)
 
-    out_file_name = "Wrapper.h"
     with open(out_file_name, "w") as generated_header:
         generated_header.write(c_file_content)
 
     print("'" + out_file_name + "' successfully generated.")
 
 def main():
-    parser = argparse.ArgumentParser(description="Generate a file containing all available wrappers of the SoftBound runtime.\n")
-    parser.add_argument('-v', '--verbose', action='store_true', help="Print verbose output")
+    """
+    Automatically generate a C++ header file that contains information on
+    available SoftBound wrappers defined by the run-time library.
+    """
+    parser = argparse.ArgumentParser(
+        description="Generate a file containing all available wrappers of the SoftBound runtime.\n")
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help="Print verbose output")
     args = parser.parse_args()
 
-    list_of_source_files = ["softboundcets-wrappers.c", "softboundcets.c", "softboundcets.h"]
+    list_of_source_files = ["softboundcets-wrappers.c",
+                            "softboundcets.c", "softboundcets.h"]
 
-    generate_file(list_of_source_files, args.verbose)
+    out_file_name = "SBWrapper.h"
+
+    generate_file(list_of_source_files, out_file_name, args.verbose)
+
+    # TODO move to include directory
 
 if __name__ == "__main__":
     main()
