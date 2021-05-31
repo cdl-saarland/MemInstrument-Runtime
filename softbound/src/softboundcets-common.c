36570580b6453c6543a88c0e7b16502872b4d3ce
Tina Jung <t.jung@cs.uni-saarland.de>
2021-05-31 10:00:53 +0200

    reorder some functions, add comment

--- softbound/src/softboundcets-common.c
+++ softbound/src/softboundcets-common.c
@@ -63,127 +63,6 @@ __WEAK_INLINE __softboundcets_trie_entry_t *__softboundcets_trie_allocate() {
     return secondary_entry;
 }
 
-__WEAK_INLINE void __softboundcets_introspect_metadata(void *ptr, void *base,
-                                                       void *bound,
-                                                       int arg_no) {
-
-    __softboundcets_printf(
-        "[introspect_metadata]ptr=%p, base=%p, bound=%p, arg_no=%d\n", ptr,
-        base, bound, arg_no);
-}
-
-__METADATA_INLINE
-void __softboundcets_copy_metadata(void *dest, const void *from, size_t size) {
-
-    __softboundcets_debug_printf("[Copy metadata] dest=%p, from=%p, size=%zx\n",
-                                 dest, from, size);
-
-    size_t dest_ptr = (size_t)dest;
-    size_t dest_ptr_end = dest_ptr + size;
-
-    size_t from_ptr = (size_t)from;
-    size_t from_ptr_end = from_ptr + size;
-
-    if (from_ptr % 8 != 0) {
-        return;
-        //    from_ptr = from_ptr %8;
-        //    dest_ptr = dest_ptr %8;
-    }
-
-    __softboundcets_trie_entry_t *trie_secondary_table_dest_begin;
-    __softboundcets_trie_entry_t *trie_secondary_table_from_begin;
-
-    size_t dest_primary_index_begin = (dest_ptr >> 25);
-    size_t dest_primary_index_end = (dest_ptr_end >> 25);
-
-    size_t from_primary_index_begin = (from_ptr >> 25);
-    size_t from_primary_index_end = (from_ptr_end >> 25);
-
-    if ((from_primary_index_begin != from_primary_index_end) ||
-        (dest_primary_index_begin != dest_primary_index_end)) {
-
-        size_t from_sizet = from_ptr;
-        size_t dest_sizet = dest_ptr;
-
-        size_t trie_size = size;
-        size_t index = 0;
-
-        for (index = 0; index < trie_size; index = index + 8) {
-
-            size_t temp_from_pindex = (from_sizet + index) >> 25;
-            size_t temp_to_pindex = (dest_sizet + index) >> 25;
-
-            size_t dest_secondary_index =
-                (((dest_sizet + index) >> 3) & 0x3fffff);
-            size_t from_secondary_index =
-                (((from_sizet + index) >> 3) & 0x3fffff);
-
-            __softboundcets_trie_entry_t *temp_from_strie =
-                __softboundcets_trie_primary_table[temp_from_pindex];
-
-            if (temp_from_strie == NULL) {
-                temp_from_strie = __softboundcets_trie_allocate();
-                __softboundcets_trie_primary_table[temp_from_pindex] =
-                    temp_from_strie;
-            }
-            __softboundcets_trie_entry_t *temp_to_strie =
-                __softboundcets_trie_primary_table[temp_to_pindex];
-
-            if (temp_to_strie == NULL) {
-                temp_to_strie = __softboundcets_trie_allocate();
-                __softboundcets_trie_primary_table[temp_to_pindex] =
-                    temp_to_strie;
-            }
-
-            void *dest_entry_ptr = &temp_to_strie[dest_secondary_index];
-            void *from_entry_ptr = &temp_from_strie[from_secondary_index];
-
-#if __SOFTBOUNDCETS_SPATIAL
-            memcpy(dest_entry_ptr, from_entry_ptr, 16);
-#elif __SOFTBOUNDCETS_TEMPORAL
-            memcpy(dest_entry_ptr, from_entry_ptr, 16);
-#elif __SOFTBOUNDCETS_SPATIAL_TEMPORAL
-            memcpy(dest_entry_ptr, from_entry_ptr, 32);
-#endif
-        }
-        return;
-    }
-
-    trie_secondary_table_dest_begin =
-        __softboundcets_trie_primary_table[dest_primary_index_begin];
-    trie_secondary_table_from_begin =
-        __softboundcets_trie_primary_table[from_primary_index_begin];
-
-    if (trie_secondary_table_from_begin == NULL)
-        return;
-
-    if (trie_secondary_table_dest_begin == NULL) {
-        trie_secondary_table_dest_begin = __softboundcets_trie_allocate();
-        __softboundcets_trie_primary_table[dest_primary_index_begin] =
-            trie_secondary_table_dest_begin;
-    }
-
-    size_t dest_secondary_index = ((dest_ptr >> 3) & 0x3fffff);
-    size_t from_secondary_index = ((from_ptr >> 3) & 0x3fffff);
-
-    assert(dest_secondary_index < __SOFTBOUNDCETS_TRIE_SECONDARY_TABLE_ENTRIES);
-    assert(from_secondary_index < __SOFTBOUNDCETS_TRIE_SECONDARY_TABLE_ENTRIES);
-
-    void *dest_entry_ptr =
-        &trie_secondary_table_dest_begin[dest_secondary_index];
-    void *from_entry_ptr =
-        &trie_secondary_table_from_begin[from_secondary_index];
-
-#if __SOFTBOUNDCETS_SPATIAL
-    memcpy(dest_entry_ptr, from_entry_ptr, 16 * (size >> 3));
-#elif __SOFTBOUNDCETS_TEMPORAL
-    memcpy(dest_entry_ptr, from_entry_ptr, 16 * (size >> 3));
-#elif __SOFTBOUNDCETS_SPATIAL_TEMPORAL
-    memcpy(dest_entry_ptr, from_entry_ptr, 32 * (size >> 3));
-#endif
-    return;
-}
-
 /* Metadata store parameterized by the mode of checking */
 
 #if __SOFTBOUNDCETS_SPATIAL
@@ -320,6 +199,128 @@ __METADATA_INLINE void __softboundcets_metadata_load(void *addr_of_ptr,
     *((lock_type *)lock) = (lock_type *)entry_ptr->lock;
 #endif
 }
+
+__METADATA_INLINE
+void __softboundcets_copy_metadata(void *dest, const void *from, size_t size) {
+
+    __softboundcets_debug_printf("[Copy metadata] dest=%p, from=%p, size=%zx\n",
+                                 dest, from, size);
+
+    size_t dest_ptr = (size_t)dest;
+    size_t dest_ptr_end = dest_ptr + size;
+
+    size_t from_ptr = (size_t)from;
+    size_t from_ptr_end = from_ptr + size;
+
+    if (from_ptr % 8 != 0) {
+        return;
+        //    from_ptr = from_ptr %8;
+        //    dest_ptr = dest_ptr %8;
+    }
+
+    __softboundcets_trie_entry_t *trie_secondary_table_dest_begin;
+    __softboundcets_trie_entry_t *trie_secondary_table_from_begin;
+
+    size_t dest_primary_index_begin = (dest_ptr >> 25);
+    size_t dest_primary_index_end = (dest_ptr_end >> 25);
+
+    size_t from_primary_index_begin = (from_ptr >> 25);
+    size_t from_primary_index_end = (from_ptr_end >> 25);
+
+    if ((from_primary_index_begin != from_primary_index_end) ||
+        (dest_primary_index_begin != dest_primary_index_end)) {
+
+        size_t from_sizet = from_ptr;
+        size_t dest_sizet = dest_ptr;
+
+        size_t trie_size = size;
+        size_t index = 0;
+
+        for (index = 0; index < trie_size; index = index + 8) {
+
+            size_t temp_from_pindex = (from_sizet + index) >> 25;
+            size_t temp_to_pindex = (dest_sizet + index) >> 25;
+
+            size_t dest_secondary_index =
+                (((dest_sizet + index) >> 3) & 0x3fffff);
+            size_t from_secondary_index =
+                (((from_sizet + index) >> 3) & 0x3fffff);
+
+            __softboundcets_trie_entry_t *temp_from_strie =
+                __softboundcets_trie_primary_table[temp_from_pindex];
+
+            if (temp_from_strie == NULL) {
+                temp_from_strie = __softboundcets_trie_allocate();
+                __softboundcets_trie_primary_table[temp_from_pindex] =
+                    temp_from_strie;
+            }
+            __softboundcets_trie_entry_t *temp_to_strie =
+                __softboundcets_trie_primary_table[temp_to_pindex];
+
+            if (temp_to_strie == NULL) {
+                temp_to_strie = __softboundcets_trie_allocate();
+                __softboundcets_trie_primary_table[temp_to_pindex] =
+                    temp_to_strie;
+            }
+
+            void *dest_entry_ptr = &temp_to_strie[dest_secondary_index];
+            void *from_entry_ptr = &temp_from_strie[from_secondary_index];
+
+#if __SOFTBOUNDCETS_SPATIAL
+            memcpy(dest_entry_ptr, from_entry_ptr, 16);
+#elif __SOFTBOUNDCETS_TEMPORAL
+            memcpy(dest_entry_ptr, from_entry_ptr, 16);
+#elif __SOFTBOUNDCETS_SPATIAL_TEMPORAL
+            memcpy(dest_entry_ptr, from_entry_ptr, 32);
+#endif
+        }
+        return;
+    }
+
+    trie_secondary_table_dest_begin =
+        __softboundcets_trie_primary_table[dest_primary_index_begin];
+    trie_secondary_table_from_begin =
+        __softboundcets_trie_primary_table[from_primary_index_begin];
+
+    if (trie_secondary_table_from_begin == NULL)
+        return;
+
+    if (trie_secondary_table_dest_begin == NULL) {
+        trie_secondary_table_dest_begin = __softboundcets_trie_allocate();
+        __softboundcets_trie_primary_table[dest_primary_index_begin] =
+            trie_secondary_table_dest_begin;
+    }
+
+    size_t dest_secondary_index = ((dest_ptr >> 3) & 0x3fffff);
+    size_t from_secondary_index = ((from_ptr >> 3) & 0x3fffff);
+
+    assert(dest_secondary_index < __SOFTBOUNDCETS_TRIE_SECONDARY_TABLE_ENTRIES);
+    assert(from_secondary_index < __SOFTBOUNDCETS_TRIE_SECONDARY_TABLE_ENTRIES);
+
+    void *dest_entry_ptr =
+        &trie_secondary_table_dest_begin[dest_secondary_index];
+    void *from_entry_ptr =
+        &trie_secondary_table_from_begin[from_secondary_index];
+
+#if __SOFTBOUNDCETS_SPATIAL
+    memcpy(dest_entry_ptr, from_entry_ptr, 16 * (size >> 3));
+#elif __SOFTBOUNDCETS_TEMPORAL
+    memcpy(dest_entry_ptr, from_entry_ptr, 16 * (size >> 3));
+#elif __SOFTBOUNDCETS_SPATIAL_TEMPORAL
+    memcpy(dest_entry_ptr, from_entry_ptr, 32 * (size >> 3));
+#endif
+    return;
+}
+
+__WEAK_INLINE void __softboundcets_introspect_metadata(void *ptr, void *base,
+                                                       void *bound,
+                                                       int arg_no) {
+
+    __softboundcets_printf(
+        "[introspect_metadata]ptr=%p, base=%p, bound=%p, arg_no=%d\n", ptr,
+        base, bound, arg_no);
+}
+
 /******************************************************************************/
 
 __WEAK_INLINE void
@@ -387,6 +388,8 @@ __softboundcets_allocation_secondary_trie_allocate(void *addr_of_ptr) {
 
     return;
 }
+
+/******************************************************************************/
 
 // Vector load/store support
 #if __SOFTBOUNDCETS_SPATIAL_TEMPORAL
